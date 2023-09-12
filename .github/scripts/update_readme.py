5b6eadd965a346ef2e54811481e0dfb45f5e66e9
Samuel Braun <office.samigo.a@gmail.com>
2023-09-12 18:01:34 +0200

    fixed code block

--- .github/scripts/update_readme.py
+++ .github/scripts/update_readme.py
@@ -19,65 +19,65 @@ if __name__ == "__main__":
 	articles_html += "</table>\n"
 
 	readme = f"""
-		<div align="center">
-			<h2>Welcome to my GitHub profile.</h2>
-			<br>
-		
-			Have a look around, you might find something useful or interesting 😁.
-			
-			<a href="https://dev.to/samuel-braun"><img src="https://img.shields.io/badge/dev.to-0A0A0A?style=for-the-badge&logo=devdotto&logoColor=white"></a>
-			<a href="https://www.webry.com/"><img src="https://img.shields.io/badge/Webry.com-1E1E1E?style=for-the-badge&logo=blog&logoColor=white"></a>
-			<a href="https://www.linkedin.com/in/samuel-braun/"><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white"></a>
-			<a href="https://bitbucket.org/samuel-braun/"><img src="https://img.shields.io/badge/Bitbucket-0747a6?style=for-the-badge&logo=bitbucket&logoColor=white"></a>
-			<a href="https://gitlab.com/braunsa/"><img src="https://img.shields.io/badge/GitLab-eb452a?style=for-the-badge&logo=gitlab&logoColor=white"></a>
-			<a href="https://ko-fi.com/samuelbraun"><img src="https://img.shields.io/badge/Ko--fi-F16061?style=for-the-badge&logo=ko-fi&logoColor=white"></a>
-			<a href="https://open.spotify.com/user/office.samigo.a"><img src="https://img.shields.io/badge/Spotify-16a349?&style=for-the-badge&logo=spotify&logoColor=white"></a>
-			
-			<br>&nbsp;
-			
-			I'm a frontend focused web developer. Here are the technologies I use the most.
+<div align="center">
+<h2>Welcome to my GitHub profile.</h2>
+<br>
 
-			<img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white">
-			<img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white">
-			<img src="https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E">
-			<img src="https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white">
-			<img src="https://img.shields.io/badge/Vue.js-35495E?style=for-the-badge&logo=vue.js&logoColor=4FC08D">
-			<img src="https://img.shields.io/badge/Tailwind-2682ab?style=for-the-badge&logo=tailwind-css&logoColor=white">
-			
-			<br>&nbsp;
-			
-			Tools I use most often.
-			
-			<img src="https://img.shields.io/badge/Vercel-000000?style=for-the-badge&logo=vercel&logoColor=white">
-			<img src="https://img.shields.io/badge/Figma-F24E1E?style=for-the-badge&logo=figma&logoColor=white">
-			<img src="https://img.shields.io/badge/Notion-000000?style=for-the-badge&logo=notion&logoColor=white">
+Have a look around, you might find something useful or interesting 😁.
 
-			&nbsp;<br>&nbsp;
-			
-			<h2>Projects & Live Demos</h2>
-			{articles_html}
-			
-			&nbsp;<br>&nbsp;
+<a href="https://dev.to/samuel-braun"><img src="https://img.shields.io/badge/dev.to-0A0A0A?style=for-the-badge&logo=devdotto&logoColor=white"></a>
+<a href="https://www.webry.com/"><img src="https://img.shields.io/badge/Webry.com-1E1E1E?style=for-the-badge&logo=blog&logoColor=white"></a>
+<a href="https://www.linkedin.com/in/samuel-braun/"><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white"></a>
+<a href="https://bitbucket.org/samuel-braun/"><img src="https://img.shields.io/badge/Bitbucket-0747a6?style=for-the-badge&logo=bitbucket&logoColor=white"></a>
+<a href="https://gitlab.com/braunsa/"><img src="https://img.shields.io/badge/GitLab-eb452a?style=for-the-badge&logo=gitlab&logoColor=white"></a>
+<a href="https://ko-fi.com/samuelbraun"><img src="https://img.shields.io/badge/Ko--fi-F16061?style=for-the-badge&logo=ko-fi&logoColor=white"></a>
+<a href="https://open.spotify.com/user/office.samigo.a"><img src="https://img.shields.io/badge/Spotify-16a349?&style=for-the-badge&logo=spotify&logoColor=white"></a>
 
-			<h2>Projects & Live Demos</h2>
-			<a href="https://frac.vercel.app/">
-				<img align="center" width="500" src="https://raw.githubusercontent.com/MindLaborDev/MindLaborDev/master/preview/Group 5.png" />
-			</a>
-			<a href="https://mindlabordev.github.io/DFT-Machine/">
-				<img align="center" width="500" src="https://raw.githubusercontent.com/MindLaborDev/MindLaborDev/master/preview/Group 4.png" />
-			</a>
-			<a href="https://github.com/MindLaborDev/gpt3-discord-chatbot">
-				<img align="center" width="500" src="https://raw.githubusercontent.com/MindLaborDev/MindLaborDev/master/preview/Group 3.png" />
-			</a>
-			<a href="https://github.com/Difinition-of-Done/bonza-commit">
-				<img align="center" width="500" src="https://raw.githubusercontent.com/MindLaborDev/MindLaborDev/master/preview/Group 6.png" />
-			</a>
-			<a href="https://github.com/MindLaborDev/skadi">
-				<img align="center" width="500" src="https://raw.githubusercontent.com/MindLaborDev/MindLaborDev/master/preview/Group 1.png" />
-			</a>
-		</div>
+<br>&nbsp;
 
-		&nbsp;<br>&nbsp;
+I'm a frontend focused web developer. Here are the technologies I use the most.
+
+<img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white">
+<img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white">
+<img src="https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E">
+<img src="https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white">
+<img src="https://img.shields.io/badge/Vue.js-35495E?style=for-the-badge&logo=vue.js&logoColor=4FC08D">
+<img src="https://img.shields.io/badge/Tailwind-2682ab?style=for-the-badge&logo=tailwind-css&logoColor=white">
+
+<br>&nbsp;
+
+Tools I use most often.
+
+<img src="https://img.shields.io/badge/Vercel-000000?style=for-the-badge&logo=vercel&logoColor=white">
+<img src="https://img.shields.io/badge/Figma-F24E1E?style=for-the-badge&logo=figma&logoColor=white">
+<img src="https://img.shields.io/badge/Notion-000000?style=for-the-badge&logo=notion&logoColor=white">
+
+&nbsp;<br>&nbsp;
+
+<h2>Projects & Live Demos</h2>
+{articles_html}
+
+&nbsp;<br>&nbsp;
+
+<h2>Projects & Live Demos</h2>
+<a href="https://frac.vercel.app/">
+	<img align="center" width="500" src="https://raw.githubusercontent.com/MindLaborDev/MindLaborDev/master/preview/Group 5.png" />
+</a>
+<a href="https://mindlabordev.github.io/DFT-Machine/">
+	<img align="center" width="500" src="https://raw.githubusercontent.com/MindLaborDev/MindLaborDev/master/preview/Group 4.png" />
+</a>
+<a href="https://github.com/MindLaborDev/gpt3-discord-chatbot">
+	<img align="center" width="500" src="https://raw.githubusercontent.com/MindLaborDev/MindLaborDev/master/preview/Group 3.png" />
+</a>
+<a href="https://github.com/Difinition-of-Done/bonza-commit">
+	<img align="center" width="500" src="https://raw.githubusercontent.com/MindLaborDev/MindLaborDev/master/preview/Group 6.png" />
+</a>
+<a href="https://github.com/MindLaborDev/skadi">
+	<img align="center" width="500" src="https://raw.githubusercontent.com/MindLaborDev/MindLaborDev/master/preview/Group 1.png" />
+</a>
+</div>
+
+&nbsp;<br>&nbsp;
 	"""
 	
 	# Write the generated README to a file (or further processing)
