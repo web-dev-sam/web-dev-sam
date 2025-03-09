477712e3c7c1bfaf2135a5c9e9f291bbb442a75a
Samuel Braun <sam@webry.com>
2025-03-09 09:51:41 +0100

    Enhance README layout with improved formatting and centralized headings

--- .github/scripts/update_readme.py
+++ .github/scripts/update_readme.py
@@ -34,33 +34,30 @@ if __name__ == "__main__":
 	articles_html += '</table>\n'
 
 	readme = f"""
+<h1 align="center">Welcome👋</h1>
+
 <br>
+
+<p align="center">
 Hey, I'm a frontend focused web developer. Have a look around, you might find something useful or interesting 😁.
+</p>
 
-<br>&nbsp;
+<br>
 
-The stack of dreams:
+<p align="center">
+  <a href="https://vuejs.org/"><img src="https://img.shields.io/badge/Vue-35495E?style=for-the-badge&logo=vue.js&logoColor=4FC08D"></a>
+  <a href="https://www.typescriptlang.org/"><img src="https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white"></a>
+  <a href="https://tailwindcss.com/"><img src="https://img.shields.io/badge/Tailwind-2682ab?style=for-the-badge&logo=tailwind-css&logoColor=white"></a>
+</p>
 
-<img src="https://img.shields.io/badge/Vue-35495E?style=for-the-badge&logo=vue.js&logoColor=4FC08D">
-<img src="https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white">
-<img src="https://img.shields.io/badge/Tailwind-2682ab?style=for-the-badge&logo=tailwind-css&logoColor=white">
+<br>
 
-<br>&nbsp;
-
-<a href="https://www.webry.com/"><img src="https://img.shields.io/badge/Webry.com-1E1E1E?style=for-the-badge&logo=blog&logoColor=white"></a>
-<a href="https://dev.to/samuel-braun"><img src="https://img.shields.io/badge/dev.to-0A0A0A?style=for-the-badge&logo=devdotto&logoColor=white"></a>
-<a href="https://www.linkedin.com/in/samuel-braun/"><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white"></a>
-<a href="https://bitbucket.org/samuel-braun/"><img src="https://img.shields.io/badge/Bitbucket-0747a6?style=for-the-badge&logo=bitbucket&logoColor=white"></a>
-
-&nbsp;<br>&nbsp;
-
-<h2>Dev.To Articles</h2>
+<h2 align="center">Dev.To Articles</h2>
 {articles_html}
 
-&nbsp;<br>&nbsp;
+<br>
 
-
-<h2>Language Stats</h2>
+<h2 align="center">Language Stats</h2>
 
 [![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=web-dev-sam&hide=htm&theme=one_dark_pro)](https://github.com/web-dev-sam)
 """
